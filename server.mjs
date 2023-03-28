e6af12e794f275bc1b14f716a23a54373c7b49c7
cxldplay <julia.muller@hotmail.no>
2023-03-28 20:33:16 +0200

    changes

--- server.mjs
+++ server.mjs
@@ -1,12 +1,17 @@
 import express from 'express'
+import * as pg from 'pg' // remove (til database)
 import apiEndpoints from './public/js/apiEndpoints.mjs';
 import encryptionAPIRoute from './routes/encryptionAPI.mjs';
 import userAPIRoute from './routes/userAPI.mjs'
+import Dictionary from './public/Dictionary.mjs';
 
 const {API_ENDPOINTS, USER_ENDPOINT}  = apiEndpoints;
 
 const server = express();
 const port = (process.env.PORT || 8080);
+server.use(express.json()) // remove (til database)
+
+const {Client} = pg.default; // remove (til database)
 
 console.log("Port set to", port);
 
